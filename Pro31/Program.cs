c25321aa594a2bfe165f0e41429eea4e3d62ece4
anshul bansal <akbt.tech17@gmail.com>
2022-08-11 09:37:57 +0530

    first commit

--- Pro31/Program.cs
+++ Pro31/Program.cs
@@ -6,7 +6,7 @@ namespace Pro31
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worl");
         }
     }
 }
